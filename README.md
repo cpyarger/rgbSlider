a214277e5c5e85d1762f03be96d1f2edda61fd43
Christopher P. Yarger <cpyarger@gmail.com>
2017-11-20 16:35:21 -0800

    first commit

---
+++ README.md
@@ -0,0 +1,8 @@
+# RGB-slider
+This is a remake of the RGB slider by Jason Forest
+
+It connects to an RGB Light over TCP and controls the RGB values
+
+it sends messages such as
+"pwm 0 12344 65535 1000"
+# rgbSlider
---
+++ index.js
@@ -0,0 +1,66 @@
+/* jshint undef: true, node: true */
+
+var express = require('express');
+//var piblaster = require('pi-blaster.js');
+var path = require('path');
+var app = express();
+
+var RED_GPIO_PIN = 17;
+var GREEN_GPIO_PIN = 18;
+var BLUE_GPIO_PIN = 22;
+
+
+//Serve public content - basically any file in the public folder will be available on the server.
+app.use(express.static(path.join(__dirname, 'public')));
+
+//We also need 3 services - Red, Green and Blue.
+// Each section is doing exactly the same but for a particular color.
+// First, we grab the value and if it is an integer we are dividing it by 255 and sending it to the pi-blaster daemon.
+var net = require('net');
+var redValue = 0;
+var greenValue =0;
+var blueValue= 0;
+var client = new net.Socket();
+client.connect(23, '192.168.42.152', function() {
+app.get('/red/:value', function (req, res) {
+    console.log("red = " + req.params.value);
+    redValue = req.params.value;
+    if( !isNaN( parseInt(redValue) ) ){
+      client.write('pwm '+redValue*255+' '+ greenValue*255 +' '+blueValue*255 +' \n');
+        //piblaster.setPwm(RED_GPIO_PIN, redValue/255);
+        res.send('ok');
+    } else {
+        res.status(400).send('error');
+    }
+});
+
+app.get('/green/:value', function (req, res) {
+    console.log("green = " + req.params.value);
+    greenValue = req.params.value;
+    if( !isNaN( parseInt(greenValue) ) ){
+      client.write('pwm '+redValue*255+' '+ greenValue*255 +' '+blueValue*255 +' \n');
+        //piblaster.setPwm(GREEN_GPIO_PIN, greenValue/255);
+        res.send('ok');
+    } else {
+        res.status(400).send('error');
+    }
+});
+
+app.get('/blue/:value', function (req, res) {
+    console.log("blue = " + req.params.value);
+    blueValue = req.params.value;
+    if( !isNaN( parseInt(blueValue) ) ){
+      client.write('pwm '+redValue*255+' '+ greenValue*255 +' '+blueValue*255 +' \n');
+        //piblaster.setPwm(BLUE_GPIO_PIN, blueValue/255);
+        res.send('ok');
+    } else {
+        res.status(400).send('error');
+    }
+});
+});
+// Start listening on port 3000.
+var server = app.listen(3000, function () {
+    var host = server.address().address;
+    var port = server.address().port;
+    console.log('RGB LED Slider listening at http://%s:%s', host, port);
+});
